c718a53d2e63ee3ae65e3e9641f01c690aa5d503
tishenme <tishenme@163.com>
2026-08-14 08:32:20 +0000

    Deployed ab91651 with MkDocs version: 1.6.1

---
+++ notes/soft/code/create.py
@@ -0,0 +1,92 @@
+import os
+
+# 定义文件夹结构
+folder_structure = {
+    '系统工具': {
+        '运行环境': [],
+        '系统优化': [],
+        '安全防护': [],
+        '数据恢复': [],
+        '硬件工具': [],
+        '窗口控制': [],
+        '文件管理': [],
+    },
+    '应用软件': {
+        '壁纸墙纸': [],
+        '日历时间': [],
+        '效率时间': [],
+        '办公软件': [],
+        '书籍阅读': [],
+        'PDF相关': [],
+        '压缩刻录': [],
+        '翻译工具': [],
+        '财务金融': [],
+        '手机管理': [],
+    },
+    '图形设计': {
+        '图片浏览': [],
+        '图像处理': [],
+        '动画制作': [],
+        '三维动画': [],
+        '交互工具': [],
+        '设计辅助': [],
+    },
+    '媒体工具': {
+        '屏幕截图': [],
+        '屏幕录像': [],
+        '音乐播放': [],
+        '音频编辑': [],
+        '视频播放': [],
+        '视频编辑': [],
+    },
+    '网络工具': {
+        '浏览器': [],
+        '下载工具': [],
+        '网络连接': [],
+        '远程控制': [],
+        '社交聊天': [],
+        '邮件客户端': [],
+        '网盘客户端': [],
+    },
+    '编程开发': {
+        '虚拟机': [],
+        '版本控制': [],
+        '编程工具': [],
+        '数据库工具': [],
+        '内网穿透': [],
+        '工具集锦': [],
+    }
+}
+
+def create_file_if_not_exists(file_path):
+    if not os.path.exists(file_path):
+        with open(file_path, 'w', encoding="utf-8") as file:
+            file.write("软件,描述,官网地址,下载渠道,包管理")
+        file.close()
+    else:
+        print(f"The file {file_path} already exists.")
+
+def replace_char_in_file(file_path, old_char, new_char):
+    # print("replace")
+    with open(file_path, 'r+', encoding="utf-8") as file:
+        content = file.read()
+        content = content.replace(old_char, new_char)
+        file.seek(0)
+        file.write(content)
+        file.truncate()
+
+# 创建文件夹和文件 ( docs\notes\soft\data )
+base_path = os.path.join(os.getcwd(), "docs", "notes", "soft", "data")
+for category, subcategories in folder_structure.items():
+    print(category)
+    print(subcategories)
+    os.makedirs(os.path.join(base_path, category), exist_ok=True)
+    for subcategory in subcategories:
+        category_path = os.path.join(base_path, category, subcategory)
+        category_path_win = category_path + "_win.csv"
+        category_path_mac = category_path + "_mac.csv"
+        create_file_if_not_exists(category_path_win)
+        create_file_if_not_exists(category_path_mac)
+        for i in range(1, 200):
+            replace_char_in_file(category_path_win, " ,", ",")
+            replace_char_in_file(category_path_mac, " ,", ",")
